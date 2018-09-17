86cc176b7f0b13863b2029e258fff6557ce1a663
ge-stalt <kareem@highground.com>
2018-09-17 17:11:11 -0500

    update

--- src/dbutil.py
+++ src/dbutil.py
@@ -20,7 +20,11 @@ def get_catalog_count():
 def get_catalogs():
     db = get_db()
     productCatalogsCollection = db['productCatalogs']
-    return productCatalogsCollection.find({})
+    return productCatalogsCollection.find({}).limit(1)
+
+def get_catalog_by_id(db, catalogId):
+    productCatalogsCollection = db['productCatalogs']
+    return productCatalogsCollection.find_one({"hgId": catalogId})
 
 def update_catalog(catalogId, status):
     db = get_db()
@@ -34,3 +38,49 @@ def update_catalog(catalogId, status):
         }
     })
     return
+
+def get_cost(value):
+    numberDecimals = len(str(value).split(".")[1])
+    remain = int(str(value)[-1:])
+    if (numberDecimals > 1 and remain > 0):
+        cost = round(value + ((10 - remain) / 100), 2)
+    else:
+        cost = value
+    return cost
+    
+def isValidProduct(data):
+    return (data["ProductName"] and
+        data["ProductId"] and
+        data["ProductCost"] and
+        data["ProductImage"] and
+        data["ProductImage"].startswith("https://") and
+        data["Popularity"] >= 0 and
+        data["ProductId"] > 0)
+
+def add_product(db, store, product, CURRENT_VERSION):
+    productCollection = db['Products']
+    productMap = {
+        "CURRENT_VERSION": CURRENT_VERSION,
+        "ModifiedDate": int(datetime.datetime.now().timestamp() * 1000),
+        "Updated": False,
+        "StoreFrontId": store["StoreFrontId"],
+        "StoreFrontName": store["StoreFrontName"],
+        "CatalogLanguage": store["CatalogLanguage"],
+        "CatalogCountry": store["CatalogCountry"],
+        "CatalogId": int(store["CatalogId"]),
+        "CatalogueId": int(store["CatalogueId"]),
+        "CatalogName": store["CatalogName"],
+        "ProductId": int(product[0]),
+        "ProductName": product[1],
+        "ProductDescription": product[2],
+        "ProductCategories": str(product[3]).split("|"),
+        "ProductBrand": product[4],
+        "ProductImage": product[5],
+        "Popularity": int(product[6]),
+        "ProductCost": int(get_cost(round(float(product[9]) + float(product[10]) + float(product[11]) + float(product[12]), 2)) * store["PointRatio"]),
+    }
+    # print(productMap)
+    if (isValidProduct(productMap)):
+        productCollection.insert_one(productMap)
+
+    
--- src/refresh_catalog.py
+++ src/refresh_catalog.py
@@ -1,20 +1,50 @@
 import os
 import requests
 import argparse
-from dbutil import update_catalog
+import codecs
+from contextlib import closing
+import csv
+from dbutil import update_catalog, add_product, get_db, get_catalog_by_id
+import datetime
 
-def refresh_catalog(catalogId, catalogUrl, token):
-    print("CatalogId: {} <---> catalogUrl: {}".format(catalogId, catalogUrl))
+def refresh_catalog(catalogId, token):
+    print("Refreshing CatalogId: {}".format(catalogId))
+
     #update status
     update_catalog(catalogId, "Inprogress")
+
     #download file
-    #save document
+    get_products(catalogId, token)
+
     #update status
+    update_catalog(catalogId, "Completed")
+
+def get_products(catalogId, token):
+    
+    CURRENT_VERSION = int(datetime.datetime.now().timestamp() * 1000)
+
+    session = requests.Session()
+
+    db = get_db()
+
+    catalog = get_catalog_by_id(db, catalogId)
+
+    with closing(session.get(catalog["CatalogCSVUrl"], stream=True, cookies={'grs': token})) as r:
+        reader = csv.reader(codecs.iterdecode(r.iter_lines(), encoding='ISO-8859-1'))
+        line_count = 0
+        for row in reader:
+            if line_count == 0:
+                line_count += 1
+            elif row != [] and len(row) > 1 and line_count > 0:
+                add_product(db, catalog, row, CURRENT_VERSION)
+                line_count += 1
+            else:
+                print(row)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Optional app description')
     parser.add_argument('--catalogId', help='A required string catalogId argument')
-    parser.add_argument('--catalogUrl', help='A required string catalogUrl argument')
     parser.add_argument('--token', help='A required string token argument')
     args = parser.parse_args()
-    refresh_catalog(args.catalogId, args.catalogUrl, args.token)
+    refresh_catalog(args.catalogId, args.token)
