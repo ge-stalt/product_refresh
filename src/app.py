4d1ab797fb2673cce705977468e31b80a24164f7
ge-stalt <kareem@highground.com>
2018-09-17 12:14:42 -0500

    update

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ import os
 import sys
 from flask import Flask
 from src.dbutil import get_catalogs
+import heroku3
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
@@ -12,32 +13,11 @@ def homepage():
     return """
     <!DOCTYPE html>
     <head>
-        <title>Product Refresh</title>
-        <link rel="stylesheet" href="http://stash.compjour.org/assets/css/foundation.css">
+        <title>Product Refresh API Server</title>
     </head>
     <body style="width: 880px; margin: auto;">  
-        <h1>Visible stuff goes here</h1>
-        <p>here's a paragraph, fwiw</p>
-        <p>And here's an image:</p>
-        <a href="https://www.flickr.com/photos/zokuga/14615349406/">
-            <img src="http://stash.compjour.org/assets/images/sunset.jpg" alt="it's a nice sunset">
-        </a>
-
-        <p>Locations:</p>
-        <a href="/places/newyork">new york</a><br/>
-        <a href="/places/stanford">stanford</a><br/>
-        <a href="/places/tokyo">tokyo</a><br/>
-
-        <p>
-            <iframe src="https://player.vimeo.com/video/105955605" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-            <p>
-                <a href="https://vimeo.com/105955605">Mary live-codes a JavaScript game from scratch &ndash; Mary Rose Cook at Front-Trends 2014
-                </a> from 
-                <a href="https://vimeo.com/fronttrends">Front-Trends</a> on <a href="https://vimeo.com">Vimeo</a>.
-            </p>
-
-        </p>
-        ENVIRONMENT:{env}
+        <h1>Product Refresh API Server</h1>
+        ENV:{env}
     </body>
     """.format(env=os.environ['BUILD_ENV'])
 
@@ -47,6 +27,7 @@ def start_refresh(catalog, token):
         os.system("python3 src/refresh_catalog.py --token {token} --catalogId {catalogId} --catalogUrl {catalogUrl}".format(token=token, catalogId=catalog["hgId"], catalogUrl=catalog["CatalogCSVUrl"]))
         print("process_catalog: {}", catalog["hgId"])
     else:
+        heroku_conn = heroku3.from_key(os.environ['HEROKU_API_KEY'])
         # refresh catalog <create a new dyno here>
         print("create new dyno")
     
